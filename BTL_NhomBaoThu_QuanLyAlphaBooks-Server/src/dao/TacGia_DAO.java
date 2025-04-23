373e077fbf360acd3b04c8cd8df15d6290d69fbb
Tan Phat <btphat11321@gmail.com>
2025-04-23 21:49:52 +0700

    add entities + dao tac gia, nha xuat ban, nha cung cap

---
+++ BTL_NhomBaoThu_QuanLyAlphaBooks-Server/src/dao/TacGia_DAO.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import connectDB.ConnectDB;
+import entities.TacGia;
+import interfaces.ITacGia;
+import jakarta.persistence.EntityManager;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author ASUS
+ */
+public class TacGia_DAO extends UnicastRemoteObject implements ITacGia {
+
+	private EntityManager em;
+
+	public TacGia_DAO() throws RemoteException {
+		em = ConnectDB.getEntityManager();
+	}
+
+	@Override
+	public List<TacGia> getAllTacGia() throws RemoteException {
+		List<TacGia> list = new ArrayList<TacGia>();
+		try {
+
+			list = em.createNamedQuery("TacGia.findAll").getResultList();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return list;
+		}
+
+		return list;
+	}
+
+	@Override
+	public List<TacGia> getTacGiaTheoID(int x) throws RemoteException {
+		throw new UnsupportedOperationException("Not supported yet."); // Generated from
+																		// nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+	}
+
+	@Override
+	public boolean addTacGia(TacGia x) throws RemoteException {
+		boolean isThisSession = em.getTransaction().isActive();
+		try {
+			if (isThisSession == false)
+				em.getTransaction().begin();
+			em.persist(x);
+			if (isThisSession == false)
+				em.getTransaction().commit();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	@Override
+	public boolean editTacGia(TacGia x) throws RemoteException {
+		throw new UnsupportedOperationException("Not supported yet."); // Generated from
+																		// nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+	}
+
+}
---
+++ BTL_NhomBaoThu_QuanLyAlphaBooks-Server/src/entities/TacGia.java
@@ -0,0 +1,92 @@
+package entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@NamedQueries({ 
+	@NamedQuery(name ="TacGia.findAll", query = "SELECT tg FROM TacGia tg"),
+	@NamedQuery(name = "TacGia.findByName", query = "SELECT tg FROM TacGia tg WHERE tg.tenTacGia = :name"),
+	@NamedQuery(name = "TacGia.findByID", query = "SELECT tg FROM TacGia tg WHERE tg.tacGiaID = :id"),
+})
+public class TacGia implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int tacGiaID;
+	@Column(name = "tenTacGia", columnDefinition = "NVARCHAR(255)")
+	private String tenTacGia;
+	@Column(name = "quocTich", columnDefinition = "NVARCHAR(255)")
+	private String quocTich;
+	
+	@OneToMany(mappedBy = "tacGia", fetch = FetchType.LAZY)
+	private List<SanPham> sanPhams;
+	
+	public int getTacGiaID() {
+		return tacGiaID;
+	}
+	public void setTacGiaID(int tacGiaID) {
+		this.tacGiaID = tacGiaID;
+	}
+	public String getTenTacGia() {
+		return tenTacGia;
+	}
+	public List<SanPham> getSanPhams() {
+		return sanPhams;
+	}
+	public void setSanPhams(List<SanPham> sanPhams) {
+		this.sanPhams = sanPhams;
+	}
+	public void setTenTacGia(String tenTacGia) throws Exception{
+		if (tenTacGia.trim().isBlank() || tenTacGia.isEmpty())
+			throw new Exception("Tên tác giả không được để trống");
+		this.tenTacGia = tenTacGia;
+	}
+	public String getQuocTich() {
+		return quocTich;
+	}
+	public void setQuocTich(String quocTich) throws Exception{
+		this.quocTich = quocTich;
+	}
+	
+	public TacGia(int tacGiaID, String tenTacGia, String quocTich) throws Exception{
+		super();
+		setQuocTich(quocTich);
+		setTacGiaID(tacGiaID);
+		setTenTacGia(tenTacGia);
+	}
+	
+        public TacGia(int tacGiaID) {
+            setTacGiaID(tacGiaID);
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(tacGiaID);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TacGia other = (TacGia) obj;
+	
+                return tacGiaID == other.tacGiaID;
+	}
+
+	public TacGia() {
+		super();
+	}
+	
+}
